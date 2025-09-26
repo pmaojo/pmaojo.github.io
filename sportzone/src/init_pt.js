6b3c5ac35691d5e3c66820ff82d24e5da303b2f1
Pelayo Maojo <pelayo.mao@gmail.com>
2025-09-26 15:20:47 +0100

    Refactor Sport Zone game assets and modal

--- sportzone/src/init_pt.js
+++ sportzone/src/init_pt.js
@@ -1,15 +1,16 @@
-var init = function(game){
-	console.log("%cCargando juego", "color:white; background:black");
+/* global SportzoneGameAssets */
+var init = function (game) {
+    console.log("%cCargando juego", "color:white; background:black");
 };
 
 init.prototype = {
-	preload: function(){
-          this.game.load.image("loading","https://pulpomarketing.com/sportzone/assets/loading.png");
-	},
-  	create: function(){
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.pageAlignHorizontally = true;
-		this.game.state.start("Preload");
-		setTimeout(1000);
-	}
-}
+    preload: function () {
+        this.game.load.image("loading", SportzoneGameAssets.resolve('loading.png'));
+    },
+    create: function () {
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.game.state.start("Preload");
+        setTimeout(1000);
+    }
+};
